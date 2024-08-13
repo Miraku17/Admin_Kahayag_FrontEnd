356afea7464b1bbfdc4570f2797725d541e3d1ca
Miraku17 <zhaztedv@gmail.com>
2024-08-13 22:27:37 +0800

    added today list component

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Breadcrumb from "@/components/breadcrumb/page";
 import SearchBar from "@/components/searchbar/page";
+import TodaySale from "@/components/TodaySale/page";
 const NavBar = () => {
   return (
     <div>
@@ -8,6 +9,9 @@ const NavBar = () => {
       <div className="flex justify-between items-center">
         <h1 className="font-bold text-3xl py-2">Main Dashboard</h1>
         <SearchBar />
+      </div>
+      <div className="today-sale-section py-4 mt-4">
+        <TodaySale />
       </div>
     </div>
   );
---
+++ src/components/todaySale/page.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { BsBarChartFill } from "react-icons/bs";
+import { TbPigMoney } from "react-icons/tb";
+import { MdSell } from "react-icons/md";
+
+const TodaySale = () => {
+  return (
+    <div className="flex justify-between gap-2">
+      <div className="left-side-section flex gap-4">
+        <div className="bg-white rounded-lg shadow-sm py-3 px-4 flex items-center gap-4 min-w-56">
+          <div className="bg-blue-100 p-2 rounded-lg">
+            <BsBarChartFill className="text-blue-600" size={20} />
+          </div>
+          <div >
+            <h2 className="text-xs text-gray-500 font-medium">Earnings</h2>
+            <p className="text-lg font-bold text-gray-800">$350.4</p>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow-sm py-3 px-4 flex items-center gap-4 min-w-56">
+          <div className="bg-blue-100 p-2 rounded-lg">
+            <TbPigMoney className="text-blue-600" size={20} />
+          </div>
+          <div>
+            <h2 className="text-xs text-gray-500 font-medium">Total Order</h2>
+            <p className="text-lg font-bold text-gray-800">500</p>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow-sm py-3 px-4 flex items-center gap-4 min-w-56">
+          <div className="bg-blue-100 p-2 rounded-lg">
+            <MdSell className="text-blue-600" size={20} />
+          </div>
+          <div>
+            <h2 className="text-xs text-gray-500 font-medium">Product Sold</h2>
+            <p className="text-lg font-bold text-gray-800">250</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="right-side-section flex gap-4">
+        <div className="bg-white rounded-lg shadow-sm py-3 px-4 flex items-center gap-4 min-w-56">
+          <div className="bg-blue-100 p-2 rounded-lg">
+            <BsBarChartFill className="text-blue-600" size={20} />
+          </div>
+          <div>
+            <h2 className="text-xs text-gray-500 font-medium">Earnings</h2>
+            <p className="text-lg font-bold text-gray-800">$350.4</p>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow-sm py-3 px-4 flex items-center gap-4 min-w-56">
+          <div className="bg-blue-100 p-2 rounded-lg">
+            <TbPigMoney className="text-blue-600" size={20} />
+          </div>
+          <div>
+            <h2 className="text-xs text-gray-500 font-medium">Total Order</h2>
+            <p className="text-lg font-bold text-gray-800">500</p>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow-sm py-3 px-4 flex items-center gap-4 min-w-56">
+          <div className="bg-blue-100 p-2 rounded-lg">
+            <MdSell className="text-blue-600" size={20} />
+          </div>
+          <div>
+            <h2 className="text-xs text-gray-500 font-medium">Product Sold</h2>
+            <p className="text-lg font-bold text-gray-800">250</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodaySale;
